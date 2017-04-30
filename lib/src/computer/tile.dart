3425d7928974c5164b2da4a1cbb08d36629a2eb3
kharland <kendaljharland@gmail.com>
2017-04-29 23:55:40 -0700

    Cleanup docs

--- lib/src/computer/tile.dart
+++ lib/src/computer/tile.dart
@@ -3,25 +3,25 @@ import 'dart:math';
 import 'package:tile_system/src/computer/hex_tile.dart';
 import 'package:tile_system/src/computer/square_tile.dart';
 
-/// A system that partitions a 2D space into a set of polygons.
-///
-/// Polygons have both an offset and a pixel position. A polygon's offset refers
-/// to its position relative to other polygons. A polygon's pixel position
-/// refers to the position of any of the pixels that intersect the polygon.
+/// A factory that partitions a 2D space into a set of polygons called tiles.
 abstract class TileFactory {
-  /// Creates a [TileFactory] made of squares.
+  /// Creates a [TileFactory] for [SquareTile].
   factory TileFactory.square(num tileRadius) = SquareTileFactory;
 
-  /// Creates a [TileFactory] made of hexagons.
+  /// Creates a [TileFactory] for [HexTile].
   factory TileFactory.hex(num tileRadius, {bool flat: false}) => flat
       ? new HexTileFactory.flat(tileRadius)
       : new HexTileFactory.pointy(tileRadius);
 
-  /// Returns the [Tile] at relative the position ([offsetX], [offsetY]).
+  /// Returns the [Tile] at the relative position [offsetX], [offsetY].
   Tile tile(int offsetX, int offsetY);
 }
 
 /// A 2D polygon.
+///
+/// A tile has both an offset and a pixel position. A tile's offset refers to
+/// its position relative to other tiles. A tile's pixel position refers to the
+/// position of any of the pixels that intersect the tile.
 abstract class Tile {
   /// The pixel center of this [Tile].
   Point<int> get center;
